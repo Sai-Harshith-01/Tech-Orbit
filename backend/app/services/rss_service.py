e67aa174029ab492a0bffc61268fb9eb7a089866
Sai-Harshith-01 <234g1a05d5@srit.ac.in>
2026-01-01 16:23:22 +0530

    Delete backend directory

--- backend/app/services/rss_service.py
+++
@@ -1,7 +0,0 @@
-from app.database import get_db
-
-db = get_db()
-
-def get_latest_news():
-    articles = list(db.rss_articles.find({}, {"_id": 0}).sort("published_at", -1).limit(10))
-    return articles
